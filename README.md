1c5089f8a69761fbd0f1b37e37a63d2a609a427a
knavesec <espringe5@sbcglobal.net>
2021-07-08 16:02:21 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Simple script to enumerate company domains via reverse WHOIS searching
+
+## Description
+
+Querying [https://viewdns.info/reversewhois/](https://viewdns.info/reversewhois/) for reverse DNS info is a great way to enumerate domains for a target company. This script automates the process of discovering these domains, and outputting information for analysis.
+
+Analyzing the WHOIS data for emails, names, addresses, states, zipcodes, etc is helpful for both gathering data and correlating domains. This script provides easy access to the information, with outputs in greppable format.
+
+## Usage
+
+```
+python3 dnsinfo.py -k "company keywords" -o outputfile.log
+```
+
+```
+python3 dnsinfo.py -k "company keywords"
+
+
+domain1.com
+    emails: abuse@godaddy.com
+    name: Target Company
+    org: Target Company
+    address: Address
+    city: None
+    state: None
+    zipcode: None
+    country: None
+```
+
+```
+python3 dnsinfo.py -k "company keywords" -g
+
+
+domain1.com : whois_emails : abuse@godaddy.com
+domain1.com : whois_name : Target Company
+domain1.com : whois_org : Target Company
+domain1.com : whois_address : Address
+domain1.com : whois_city : None
+domain1.com : whois_state : None
+domain1.com : whois_zipcode : None
+domain1.com : whois_country : None
+```
---
+++ dnsinfo.py
@@ -0,0 +1,77 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import whois
+import argparse
+
+
+def dnsinfo(options):
+
+    entry = '+'.join(options.keywords.split(' '))
+
+    site = "https://viewdns.info/reversewhois/?q={entry}".format(entry=entry)
+    headers = {
+        'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "close",
+        "Upgrade-Insecure-Requests": "1"
+    }
+
+    r = requests.get(site,headers=headers)
+
+    dat = []
+
+    soup = bs(r.text, "html.parser")
+
+    tables = soup.find("table", border=1)
+    try:
+        for tr in tables.find_all("tr"):
+            dat.append( tr.find_all("td")[0].text )
+    except AttributeError as e:
+        print("No domains returned, exiting...")
+        return
+
+    dat = dat[1:len(dat)]
+
+    values = ["emails", "name", "org", "address", "city", "state", "zipcode", "country"]
+    for domain in dat:
+        try:
+            w = whois.whois(domain)
+            if not options.greppable:
+                output(options, domain + ":")
+            for val in values:
+                if not options.greppable:
+                    output(options, '\t{val}: {data}'.format(val=val, data=w[val]))
+                else:
+                    output(options, '{domain} : whois_{val} : {data}'.format(domain=domain,val=val,data=w[val]))
+        except:
+            pass
+
+
+def output(options, outstr):
+
+    if options.outfile == "":
+        print(outstr)
+    else:
+        f = open(options.outfile,'a+')
+        f.write(outstr)
+        f.write('\n')
+        f.close()
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="yeet, todo")
+
+    parser.add_argument('-k', '--keywords', default="", required=True, help="words to search for")
+    parser.add_argument('-g', '--greppable', default=False, required=False, action="store_true", help="output in greppable format")
+    parser.add_argument('-o', '--outfile', default="", required=False, help="output filename, else stdout")
+
+    options = parser.parse_args()
+
+    dnsinfo(options)
+
+
+if __name__ == '__main__':
+    main()
