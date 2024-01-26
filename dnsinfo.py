ce3e61d37e73c5d300b38a7bff059c0db74f20e1
Ellis Springe <ellis.springe@ibm.com>
2024-01-26 13:44:37 -0700

    add option for file lookups

--- dnsinfo.py
+++ dnsinfo.py
@@ -6,52 +6,62 @@ import argparse
 
 def dnsinfo(options):
 
-    entry = '+'.join(options.keywords.split(' '))
-
-    site = "https://viewdns.info/reversewhois/?q={entry}".format(entry=entry)
-    headers = {
-        'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "Connection": "close",
-        "Upgrade-Insecure-Requests": "1"
-    }
-
-    r = requests.get(site,headers=headers)
-
     dat = []
 
-    soup = bs(r.text, "html.parser")
+    if options.file == "":
+        entry = '+'.join(options.keywords.split(' '))
 
-    tables = soup.find("table", border=1)
-    try:
-        for tr in tables.find_all("tr"):
-            dat.append( tr.find_all("td")[0].text )
-    except AttributeError as e:
-        print("No domains returned, exiting...")
-        return
+        site = "https://viewdns.info/reversewhois/?q={entry}".format(entry=entry)
+        headers = {
+            'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate",
+            "Connection": "close",
+            "Upgrade-Insecure-Requests": "1"
+        }
 
-    dat = dat[1:len(dat)]
+        r = requests.get(site,headers=headers)
 
+        soup = bs(r.text, "html.parser")
+
+        tables = soup.find("table", border=1)
+        try:
+            for tr in tables.find_all("tr"):
+                dat.append( tr.find_all("td")[0].text )
+        except AttributeError as e:
+            print("No domains returned, exiting...")
+            return
+
+        dat = dat[1:len(dat)]
+
+
+    else: 
+
+        dat = open(options.file, 'r').readlines()
+
+    lookup_domains(dat, options)
+
+
+def lookup_domains(dat, options):
     # values = ["emails", "name", "org", "address", "city", "state", "zipcode", "country"]
     for domain in dat:
         try:
-            w = whois.whois(domain)
+            w = whois.whois(domain.strip())
             if not options.greppable:
-                output(options, domain + ":")
+                output(options, domain.strip() + ":")
             for val in w:
                 try:
                     if not options.greppable:
                         output(options, '\t{val}: {data}'.format(val=val, data=w[val]))
                     else:
-                        output(options, '{domain} : whois_{val} : {data}'.format(domain=domain,val=val,data=w[val]))
+                        output(options, '{domain} : whois_{val} : {data}'.format(domain=domain.strip(),val=val,data=w[val]))
                 except:
                     pass
-            print("Complete: {domain}".format(domain=domain))
+            print("Complete: {domain}".format(domain=domain.strip()))
         except:
 
-            print("Error with domain: {domain}".format(domain=domain))
+            print("Error with domain: {domain}".format(domain=domain.strip()))
             pass
 
 
@@ -70,9 +80,10 @@ def main():
 
     parser = argparse.ArgumentParser(description="yeet, todo")
 
-    parser.add_argument('-k', '--keywords', default="", required=True, help="words to search for")
+    parser.add_argument('-k', '--keywords', default="", required=False, help="words to search for")
     parser.add_argument('-g', '--greppable', default=False, required=False, action="store_true", help="output in greppable format")
     parser.add_argument('-o', '--outfile', default="", required=False, help="output filename, else stdout")
+    parser.add_argument('-f', '--file', default="", required=False, help="input filename, for specific domain queries")
 
     options = parser.parse_args()
 
