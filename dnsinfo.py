1a99cd10ecd19c4984079c31b4f392f4faa8b129
Ellis Springe <ellis.springe@ibm.com>
2025-12-02 05:36:18 -0700

    file of keyword support added

--- dnsinfo.py
+++ dnsinfo.py
@@ -7,33 +7,43 @@ def dnsinfo(options):
 
     dat = []
 
-    entry = '+'.join(options.keywords.split(' '))
+    keywords = []
+    if options.keyword_file != "":
+        with open(options.keyword_file, 'r') as f:
+            keywords = f.readlines()
+    else:
+        keywords = [options.keywords]
 
-    site = "https://viewdns.info/reversewhois/?q={entry}&t=1".format(entry=entry)
-    headers = {
-        'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "Connection": "close",
-        "Upgrade-Insecure-Requests": "1"
-    }
+    for keyword in keywords:
 
-    r = requests.get(site,headers=headers)
+        print(f"Searching for: {keyword.strip()}")
+        entry = '+'.join(keyword.strip().split(' '))
 
-    soup = bs(r.text, "html.parser")
+        site = "https://viewdns.info/reversewhois/?q={entry}&t=1".format(entry=entry)
+        headers = {
+            'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate",
+            "Connection": "close",
+            "Upgrade-Insecure-Requests": "1"
+        }
 
-    c = soup.find_all("td", {"class":"px-6 py-4 whitespace-nowrap text-base font-medium text-gray-900 dark:text-gray-100"})
+        r = requests.get(site,headers=headers)
 
-    #tables = soup.find("table", border=1)
-    try:
-        for i in c:
-            dat.append(i.text)
-        # for tr in tables.find_all("tr"):
-        #     dat.append( tr.find_all("td")[0].text )
-    except AttributeError as e:
-        print("No domains returned, exiting...")
-        return
+        soup = bs(r.text, "html.parser")
+
+        c = soup.find_all("td", {"class":"px-6 py-4 whitespace-nowrap text-base font-medium text-gray-900 dark:text-gray-100"})
+
+        #tables = soup.find("table", border=1)
+        try:
+            for i in c:
+                dat.append(i.text)
+            # for tr in tables.find_all("tr"):
+            #     dat.append( tr.find_all("td")[0].text )
+        except AttributeError as e:
+            print("No domains returned, exiting...")
+            return
 
     for d in dat: 
         if options.outfile != "":
@@ -58,6 +68,7 @@ def main():
     parser = argparse.ArgumentParser(description="perform keyword based reverse whois searches via viewdns.info")
 
     parser.add_argument('-k', '--keywords', default="", required=False, help="words to search for")
+    parser.add_argument('-kf', '--keyword-file', default="", required=False, help="file containing words to search for")
     parser.add_argument('-o', '--outfile', default="", required=False, help="output filename, else stdout")
 
     options = parser.parse_args()
@@ -67,3 +78,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
--- whoisxmlapi.py
+++ whoisxmlapi.py
@@ -6,23 +6,36 @@ import json
 def reverse(options):
     url = "https://reverse-whois.whoisxmlapi.com/api/v2"
 
-    data = {
-        "apiKey": options.apikey,
-        "searchType": "current",
-        "mode": "purchase",
-        "punycode": True,
-        "basicSearchTerms": {
-            "include": options.keywords,
-            "exclude": [
-            ]
+    dat = [] 
+    keywords = []
+    if options.keyword_file != "":
+        with open(options.keyword_file, 'r') as f:
+            keywords = f.readlines()
+    else:
+        keywords = [options.keywords]
+
+    for keyword in keywords:
+        print(f"Searching for: {keyword.strip()}")
+
+        data = {
+            "apiKey": options.apikey,
+            "searchType": "current",
+            "mode": "purchase",
+            "punycode": True,
+            "basicSearchTerms": {
+                "include": [keyword.strip()],
+                "exclude": [
+                ]
+            }
         }
-    }
 
+        r = requests.post(url=url, json=data)
+        j = json.loads(r.text)
 
-    r = requests.post(url=url, json=data)
-    j = json.loads(r.text)
+        for d in j['domainsList']: 
+            dat.append(d.strip())
 
-    for d in j['domainsList']: 
+    for d in dat: 
         if options.outfile != "":
             output(options, d.strip())
         else:
@@ -45,6 +58,7 @@ def main():
     parser = argparse.ArgumentParser(description="perform keyword based reverse whois searches via viewdns.info")
 
     parser.add_argument('-k', '--keywords', default=[], action='append', required=False, help="words to search for")
+    parser.add_argument('-kf', '--keyword-file', default="", required=False, help="file containing words to search for")
     parser.add_argument('-o', '--outfile', default="", required=False, help="output filename, else stdout")
     parser.add_argument('--apikey', default="", required=True, help="whoisxml api key")
 
